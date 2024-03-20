73279d3af963bc3a1a66fdd0a360981d21f87ace
Mathis <mathis.reclus@gmail.com>
2024-03-20 18:09:59 +0100

    avancement authentification client -server websocket

--- akevisionagent/linux/agent_script.py
+++ akevisionagent/linux/agent_script.py
@@ -4,16 +4,23 @@ import json
 import time
 
 async def connect_to_server():
-    pc_id = 1  # Remplacez cela par la logique pour obtenir ou générer l'ID du PC
-    uri = f"ws://localhost:8000/ws/client/2/"
+    pc_id = 3  # Remplacez cela par la logique pour obtenir ou générer l'ID du PC
+    uri = f"ws://localhost:8000/ws/client/{pc_id}/"
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOjMsImV4cCI6MTcxMTAzOTcxNn0.r_kg63zlKAZUYJfX4kXRuqTsIDAPCIcslC60H14iEP4'
 
-    async with websockets.connect(uri) as websocket:
+    # Ajoutez l'en-tête d'authentification
+    headers = {
+        "Authorization": token
+    }
+
+    async with websockets.connect(uri, extra_headers=headers) as websocket:
         while True:
-            message = {"data": {"token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOjIsImV4cCI6MTcxMDU5OTQwNH0.HWRDMMXZWvq5MLcXQDHwNtUsCRVCAjuCdJECtN_Q99k', "ram": 80, "storage": 60}}
+            message = {'ram' : 60, 'cpu' : 20, 'test' : 'test'}
             await websocket.send(json.dumps(message))
+
             response = await websocket.recv()
             print(f"Received from server: {response}")
-            
+
             # Pause de 2 secondes avant d'envoyer la prochaine mise à jour
             await asyncio.sleep(5)
 
--- akevisionback/akevision_rest/consumers.py
+++ akevisionback/akevision_rest/consumers.py
@@ -8,33 +8,32 @@ class ClientWebsocketConsumer(AsyncWebsocketConsumer):
         url_parts = self.scope['url_route']['kwargs']
         client_id = url_parts['client_id']
         print(client_id)
-        print(self.receive)
-        print(self.__dict__)
-        # Attendre de recevoir des données du client
 
-        data = await self.receive()
-        if data is None:
-            print('Aucune donnée reçue')
+        token = None
+        for header, value in self.scope['headers']:
+            if header.decode() == 'authorization':
+                token = value.decode()
+                break
+        print(token)
+
+        if not token:
+            print('Jeton d\'authentification manquant')
             await self.close()
             return
 
+        # Vérifier le jeton d'authentification
         try:
-            data = json.loads(data)
-            access_token = data['token']
-        except (TypeError, KeyError) as e:
-            print('Données invalides')
-            await self.close()
-            return
-
-        try:
-            payload = TokenService.decode_access_token(access_token)
+            payload = TokenService.decode_access_token(token)
             if payload['client_id'] != client_id:
+                print(f"Le jeton n'appartient pas au client {client_id}")
                 await self.close()
                 return
 
             client = Client.objects.get(id=client_id)
+            print(f"Client {client.name} trouvé en base de données")
 
             if client.ipv4 != self.scope['client'][0]:
+                print(f"L'adresse IP de la requête ({self.scope['client'][0]}) ne correspond pas à l'adresse IP du client ({client.ipv4})")
                 await self.close()
                 return
 
@@ -43,6 +42,11 @@ class ClientWebsocketConsumer(AsyncWebsocketConsumer):
             client.save()
 
             await self.accept()
+
+        except Client.DoesNotExist:
+            print(f"Le client {client_id} n'existe pas en base de données")
+            await self.close()
+            return
 
         except Exception as e:
             print(e)
